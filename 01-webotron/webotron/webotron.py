067268e518663b1129f8453609b37499326be566
Wieslaw Pedzich <wieslaw.pedzich@gmail.com>
2019-12-30 19:20:25 +0100

    Sync directory tree to bucket

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,29 +1,33 @@
 import boto3
 import click
 from botocore.exceptions import ClientError
+from pathlib import Path
+import mimetypes
 
 session = boto3.Session(profile_name='default')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "Webotron deploys website to AWS"
+    "Webotron deploys websites to AWS"
     pass
 
 
 @cli.command('list-buckets')
 def list_buckets():
-    "list all s3 buckets"
+    "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
 
 
-@cli.command('list-buckets-objects')
+@cli.command('list-bucket-objects')
 @click.argument('bucket')
-def list_buckets_objects(bucket):
-    "list objects in an s3 bucket"
+def list_bucket_objects(bucket):
+    "List objects in an s3 bucket"
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
@@ -71,6 +75,35 @@ def setup_bucket(bucket):
 
     return
 
+
+def upload_file(s3_bucket, path, key):
+    content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+
+    s3_bucket.upload_file(
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': 'text/html'
+        })
+
+
+@cli.command('sync')
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucket')
+def sync(pathname, bucket):
+    "Sync contents of PATHNAME to BUCKET"
+    s3_bucket = s3.Bucket(bucket)
+
+    root = Path(pathname).expanduser().resolve()
+
+    def handle_directory(target):
+        for p in target.iterdir():
+            if p.is_dir():
+                handle_directory(p)
+            if p.is_file():
+                upload_file(s3_bucket, str(p), str(p.relative_to(root)))
+
+    handle_directory(root)
 
 
 if __name__ == '__main__':
